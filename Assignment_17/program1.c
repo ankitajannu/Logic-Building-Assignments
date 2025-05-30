206feddbd0719af9e23e102bc62f94531530f498
ankitajannu <ankitajannu11@gmail.com>
2025-05-30 23:57:49 +0530

    Add files via upload

---
+++ Assignment_17/program1.c
@@ -0,0 +1,50 @@
+// Input : iRow = 4, iCol = 4
+// Output : 
+/*
+    * * * #
+    * * # *
+    * # * * 
+    # * * *
+*/
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid input");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if((i > j) || (i < j))
+            {
+                printf("*\t");
+            }
+            if(i == j)
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
