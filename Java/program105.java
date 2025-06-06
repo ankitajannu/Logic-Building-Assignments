15727522430970bc121c8b6aa883eef25832c86e
ankitajannu <ankitajannu11@gmail.com>
2025-06-06 17:30:26 +0530

    Add files via upload

---
+++ Java/program105.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if((j == 1) || (j == iCol) || (i == 1) || (i == iRow))
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("o\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program105
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        int iValue1 = 0, iValue2 = 0;
+       
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue1 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);  
+    }
+}
+
