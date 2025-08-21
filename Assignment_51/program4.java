642a70392db75807bda1d1ba89a5d77f69e18491
ankitajannu <ankitajannu11@gmail.com>
2025-08-21 23:05:58 +0530

    Add files via upload

---
+++ Assignment_51/program4.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class Matrix
+{
+    private int Arr[][];
+
+    public Matrix(int A, int B)
+    {
+        Arr = new int[A][B];
+    }
+
+    public void Accept()
+    {
+        System.out.println("Please enter the elements of the matrix : ");
+
+        Scanner sobj = new Scanner(System.in);
+
+        int i = 0, j = 0;
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            for(j = 0; j < Arr[i].length; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+    }
+
+    public void Display()
+    {
+        System.out.println("Elements of the matrix are : ");
+
+        int i = 0, j = 0;
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            for(j = 0; j < Arr[i].length; j++)
+            {
+                System.out.print(Arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public int Trace()
+    {
+        int i = 0, j = 0, iSum = 0;
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            for(j = 0; j < Arr[i].length; j++)
+            {
+                if(i == j)
+                {
+                    iSum = iSum + Arr[i][j];
+                }  
+            }
+        }
+        return iSum;
+    }
+
+    public double Normal()
+    {
+        int i = 0, j = 0, iSum = 0;
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            for(j = 0; j < Arr[i].length; j++)
+            {
+                iSum = iSum + Arr[i][j] * Arr[i][j];
+            }
+        }
+        return Math.sqrt(iSum);
+    }
+}
+
+class program4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iRet = 0;
+        double dRet = 0;
+
+        System.out.println("Enter number of rows : ");
+        int iRow = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        int iCol = sobj.nextInt();
+
+        Matrix mobj = new Matrix(iRow,iCol);
+
+        mobj.Accept();
+
+        mobj.Display();
+
+        iRet = mobj.Trace();
+        System.out.println("Trace is : "+iRet);
+
+        dRet = mobj.Normal();
+        System.out.println("Normal is : "+dRet);
+    }   
+}
