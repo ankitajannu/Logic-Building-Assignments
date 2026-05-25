75da75397e2432320dca152255921cd64ce6c7d1
ankitajannu <ankitajannu11@gmail.com>
2026-05-25 15:43:14 +0530

    Delete Java directory

--- Java/Program1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Program1
-{
-    public static void main(String A[])
-    {
-        System.out.println("Jay Ganesh...");
-    }
-}
--- Java/Program51.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Program51
-{
-    public static void main(String A[])
-    {
-        System.out.println("Jay Ganesh...");
-    }
-}
